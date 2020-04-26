7ec1ab8a81e28eff875a1e4081e1bb0dff95e313
liweiyap <liweiyap@gmail.com>
2020-04-26 21:57:54 +0100

    Add default grid

--- gridwidget.cpp
+++ gridwidget.cpp
@@ -5,6 +5,37 @@
 GridWidget::GridWidget(QWidget* parent) : QWidget(parent)
 {
     universeBorderColour.setAlpha(255);
+    universeFieldColour.setAlpha(255);
+
+    grid = new int*[getRowCount()];
+    for (size_t rowIdx = 0; rowIdx < getRowCount(); ++rowIdx)
+    {
+        grid[rowIdx] = new int[getColumnCount()];
+        for (size_t columnIdx = 0; columnIdx < getColumnCount(); ++columnIdx)
+        {
+            grid[rowIdx][columnIdx] = ((rowIdx + columnIdx) & 1) ? 0 : 1;
+        }
+    }
+}
+
+size_t GridWidget::getRowCount() const
+{
+    return rowCount;
+}
+
+void GridWidget::setRowCount(const size_t nRows)
+{
+    rowCount = nRows;
+}
+
+size_t GridWidget::getColumnCount() const
+{
+    return columnCount;
+}
+
+void GridWidget::setColumnCount(const size_t nColumns)
+{
+    columnCount = nColumns;
 }
 
 QColor GridWidget::getUniverseBorderColour() const
@@ -27,11 +58,43 @@ void GridWidget::setUniverseBorderThickness(const qreal thickness)
     universeBorderThickness = thickness;
 }
 
+QColor GridWidget::getUniverseFieldColour() const
+{
+    return universeFieldColour;
+}
+
+void GridWidget::setUniverseFieldColour(const QColor colour)
+{
+    universeFieldColour = colour;
+}
+
 void GridWidget::paintEvent(QPaintEvent* event)
 {
     QPainter painter(this);
 
     QRect universeBorder(0, 0, width(), height());
+    painter.setBrush(QBrush(getUniverseFieldColour()));
+    painter.fillRect(universeBorder, painter.brush());
     painter.setPen(QPen(getUniverseBorderColour(), getUniverseBorderThickness()));
     painter.drawRect(universeBorder);
+
+    qreal universeWidth = width() - 1.5 * getUniverseBorderThickness();
+    qreal universeHeight = height() - 1.5 * getUniverseBorderThickness();
+    qreal cellWidth = universeWidth / getRowCount();
+    qreal cellHeight = universeHeight / getColumnCount();
+
+    for (size_t rowIdx = 0; rowIdx < getRowCount(); ++rowIdx)
+    {
+        for (size_t columnIdx = 0; columnIdx < getColumnCount(); ++columnIdx)
+        {
+            if (grid[rowIdx][columnIdx] == 1)
+            {
+                qreal cellLeftIdx = 0.75 * getUniverseBorderThickness() + cellWidth * columnIdx;
+                qreal cellTopIdx = 0.75 * getUniverseBorderThickness() + cellHeight * rowIdx;
+                QRect cellBorder(cellLeftIdx, cellTopIdx, cellWidth, cellHeight);
+                painter.setBrush(QBrush(Qt::black));
+                painter.fillRect(cellBorder, painter.brush());
+            }
+        }
+    }
 }
--- gridwidget.h
+++ gridwidget.h
@@ -8,12 +8,21 @@ namespace Ui { class gridwidget; }
 class GridWidget : public QWidget
 {
     Q_OBJECT
+    Q_PROPERTY(size_t rowCount READ getRowCount WRITE setRowCount DESIGNABLE true)
+    Q_PROPERTY(size_t columnCount READ getColumnCount WRITE setColumnCount DESIGNABLE true)
     Q_PROPERTY(QColor universeBorderColour READ getUniverseBorderColour WRITE setUniverseBorderColour DESIGNABLE true)
     Q_PROPERTY(qreal universeBorderThickness READ getUniverseBorderThickness WRITE setUniverseBorderThickness DESIGNABLE true)
+    Q_PROPERTY(QColor universeFieldColour READ getUniverseFieldColour WRITE setUniverseFieldColour DESIGNABLE true)
 
 public:
     explicit GridWidget(QWidget* parent = nullptr);
     ~GridWidget() = default;
+
+    size_t getRowCount() const;
+    void setRowCount(const size_t nRows);
+
+    size_t getColumnCount() const;
+    void setColumnCount(const size_t nColumnss);
 
     QColor getUniverseBorderColour() const;
     void setUniverseBorderColour(const QColor colour);
@@ -21,12 +30,22 @@ public:
     qreal getUniverseBorderThickness() const;
     void setUniverseBorderThickness(const qreal thickness);
 
+    QColor getUniverseFieldColour() const;
+    void setUniverseFieldColour(const QColor colour);
+
 protected:
     void paintEvent(QPaintEvent* event);
 
 private:
+    /* game settings */
+    size_t rowCount = 20;
+    size_t columnCount = 20;
+    int** grid;
+
+    /* design settings */
     QColor universeBorderColour = "#3873b3";
-    qreal universeBorderThickness = 6.0;
+    qreal universeBorderThickness = 8.0;
+    QColor universeFieldColour = Qt::white;
 };
 
 #endif // GRIDWIDGET_H
